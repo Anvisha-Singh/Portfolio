84eb9432e9eb39c348a1a86d546804a8370e4bc6
Anvisha Singh <anvishasingh@Anvishas-MacBook-Air.local>
2025-11-18 20:33:52 -0500

    test

--- my-portfolio/src/components/Hero.tsx
+++ my-portfolio/src/components/Hero.tsx
@@ -39,8 +39,8 @@ export default function Hero() {
       radius: number;
 
       constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.x = Math.random() * canvas!.width;
+        this.y = Math.random() * canvas!.height;
         this.vx = (Math.random() - 0.5) * 0.5;
         this.vy = (Math.random() - 0.5) * 0.5;
         this.radius = Math.random() * 2 + 1;
@@ -50,8 +50,8 @@ export default function Hero() {
         this.x += this.vx;
         this.y += this.vy;
 
-        if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
-        if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+        if (this.x < 0 || this.x > canvas!.width) this.vx *= -1;
+        if (this.y < 0 || this.y > canvas!.height) this.vy *= -1;
       }
 
       draw(context: CanvasRenderingContext2D) {
